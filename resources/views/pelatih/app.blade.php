b55cca93d06fe39bccee51af313448e6c2dfa741
Afwan <tmariefafwan@gmail.com>
2023-05-20 14:51:35 +0700

    fixing

--- resources/views/pelatih/app.blade.php
+++ resources/views/pelatih/app.blade.php
@@ -18,7 +18,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <link href="{{ asset('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
